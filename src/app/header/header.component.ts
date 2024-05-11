70893c5a395f0fbe0962637fb5cb4c21561c019a
khandokarsabbir@gmail.com <khandokarsabbir@gmail.com>
2024-05-11 10:12:39 -0700

    First commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { TopMenuComponent } from "./top-menu/top-menu.component";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    styleUrl: './header.component.css',
+    standalone: true,
+    imports:[TopMenuComponent, MainMenuComponent]
+})
+
+export class HeaderComponent{
+
+}
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from './search/search.component';
+
+@Component({
+  selector: 'product-list',
+  standalone: true,
+  imports: [SearchComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  //name = 'John Doe';
+  addToCart:number = 0;
+  product = {
+    name: 'iPhone X',
+    price: 789,
+    color: 'Black',
+    discount: 8.5,
+    inStock: 10,
+    pImage: '/assets/images/iphone.png'
+  }
+
+  getDiscountedPrice() {
+    return this.product.price - (this.product.price * this.product.discount / 100)
+  }
+
+  onNameChange(event:any){
+    //this.name = event.target.value;
+    //console.log(event);
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+    
+  }
+
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
+    
+  }
+
+}
