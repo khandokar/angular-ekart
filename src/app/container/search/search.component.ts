afc5dcfdb39a7ea58d993011bbc691db66515635
khandokarsabbir@gmail.com <khandokarsabbir@gmail.com>
2024-05-12 14:05:02 -0700

    Add ngIf

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,15 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchText: string = 'Womens watch';
+  searchText: string = '';
 
   updateSearchText(event: any){
       this.searchText = event.target.value;
