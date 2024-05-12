e47bd9fc266999c0b4f45d1137c43c8cd6f82949
khandokarsabbir@gmail.com <khandokarsabbir@gmail.com>
2024-05-12 13:12:56 -0700

    Product List Component added

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, ProductListComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
