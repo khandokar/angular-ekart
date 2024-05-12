667b5316e5a53b01fa36d964fd4ba5cdd07df3c1
khandokarsabbir@gmail.com <khandokarsabbir@gmail.com>
2024-05-12 11:35:22 -0700

    ngFor added on Menu

--- src/app/header/main-menu/main-menu.component.ts
+++ src/app/header/main-menu/main-menu.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'main-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
 export class MainMenuComponent {
+  mainMenuItems : string[] = ['Home','Products','About','Contact']
 
 }
