819ad230f167e0315f4c9406c886fa51a8d89fff
khandokarsabbir@gmail.com <khandokarsabbir@gmail.com>
2024-06-01 14:34:05 -0700

    Implement Input decorator

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ProductComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
