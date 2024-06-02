c0296c5603d1344ec6f0ad412b4a0db06fa7dce5
khandokarsabbir@gmail.com <khandokarsabbir@gmail.com>
2024-06-01 17:44:27 -0700

    Implement filter using Output decorator and customer event binding

--- src/app/container/product-list/filter/filter.component.ts
+++ src/app/container/product-list/filter/filter.component.ts
@@ -1,18 +1,33 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
   @Input()
   all: number = 0;
+
   @Input()
   inStock: number = 0;
+
   @Input()
   outOfStock: number = 0;
 
+  //Create the event
+  @Output()
+  selectedFilterRadioButtonChanged:  EventEmitter<string> = new EventEmitter<string>()
+  
+  selectedFilterRadioButton: string = 'all';
+
+  //Method to raise the event
+  onSelectedFilterRadioButtonChanged(){
+    //Raise the event
+    this.selectedFilterRadioButtonChanged.emit(this.selectedFilterRadioButton);
+  }
+  
 }
